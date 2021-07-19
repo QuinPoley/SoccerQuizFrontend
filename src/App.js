78be889fed42d3eaaae684a4a68a99ee692f2be0
QuinPoley <quin.poley@gmail.com>
2021-07-19 17:11:39 -0400

    Quiz and radio buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,58 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import SelectQuizDropDown from './SelectQuizDropDown';
 import {  ReactComponent as Caret } from './Icons/caret.svg';
+import { quiz } from './quiz.json';
+import { func } from 'prop-types';
 
+
+function getQuiz(quiz){
+  return(null);
+}
 
 function makeApiCall(selectedquiz){
-  return (<div>
-      You have selected {selectedquiz}
-      </div>
-      )
+  //const thequiz = JSON.parse(quiz);
+  /*<ul>
+                        {valuesArray.map(item => {
+                            return <li>{item}</li>;
+                        })}
+  </ul>
+  
+  <div>
+          You have selected {selectedquiz}
+  </div>*/
+  return (
+        <ul className="questionlist">
+            {quiz.map(item => {
+                return(
+                <li>
+                  <div className="quest">{item.text}</div>
+                    <input type="radio" id={item.text + item.A} name={item.text} value={item.A}/>
+                    <label for={item.text + item.A}>{item.A}</label><br/>
+                    <input type="radio" id={item.text + item.B} name={item.text} value={item.A}/>
+                    <label for={item.text + item.B}>{item.B}</label><br/>
+                    <input type="radio" id={item.text + item.C} name={item.text} value={item.A}/>
+                    <label for={item.text + item.C}>{item.C}</label><br/>
+                    <input type="radio" id={item.text + item.D} name={item.text} value={item.A}/>
+                    <label for={item.text + item.D}>{item.D}</label>
+                  <p/>
+                </li>
+                );
+            })}
+        </ul>
+      );
+}
+function getQuizResult(){
+  for (let i = 0; i < quiz.length; i++) {
+    var elem = document.getElementsByName(quiz[i].text);
+    for (let j = 0; j < elem.length; j++) {
+      if(elem[j].checked){
+        console.log(elem[j]);
+      }
+    }
+  }
+  
+  document.getElementById("putresulthere").append("10/10 GREAT WORK");
+  // Do another API call to get answers
 }
 
 function Quiz({selectedquiz}) {
@@ -17,6 +62,8 @@ function Quiz({selectedquiz}) {
   return (
     <div className="questions">
         {response}
+        <div className="submit" onClick={() => getQuizResult()}>Submit</div>
+        <div className="result" id="putresulthere"></div>
     </div>
   );
 }
